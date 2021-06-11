0a81336be0c6cce47ecbb3ef6c1ce4ccfe672d95
ABojo <alex.bojorquez2598@gmail.com>
2021-06-10 21:51:56 -0400

    created a basic navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div className="text-white bg-true-gray-800 p-5 flex justify-center">
+      <div>
+        <div className="text-3xl inline-block mr-3">
+          <i class="fas fa-bomb"></i>
+        </div>
+
+        <h1 className="text-3xl inline-block font-bold">WZTracker</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
