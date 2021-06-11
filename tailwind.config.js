fa9c67ae6641c47e916e6117af3c7a3229416f7e
ABojo <alex.bojorquez2598@gmail.com>
2021-06-10 21:13:30 -0400

    Added colors to tailwind pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       sans: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+    },
+    extend: {
+      colors: {
+        'true-gray': colors.trueGray,
+        'warm-gray': colors.warmGray,
+        'cool-gray': colors.coolGray,
+        'alt-gray': colors.gray,
+        'blue-gray': colors.blueGray,
+      },
     },
   },
   variants: {
