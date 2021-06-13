2ab89a91453e6022645addb99357a470043dd475
ABojo <alex.bojorquez2598@gmail.com>
2021-06-12 20:34:48 -0400

    added a spinner for when the data is loading

--- src/components/ProfileBox.js
+++ src/components/ProfileBox.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import StatsGrid from './StatsGrid';
 import Heading from './Heading';
+import Spinner from './Spinner';
 import React from 'react';
 
 function ProfileBox() {
   const { platform, username } = useParams();
   const [userData, setUserData] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getUserData = async () => {
@@ -18,24 +20,35 @@ function ProfileBox() {
       if (json.status === 'success') {
         setUserData(json.data);
       }
+
+      setIsLoading(false);
     };
 
     getUserData();
   }, []);
 
-  return (
-    <React.Fragment>
-      <Heading
-        headingText={`${username} Warzone Player Card`}
-        subText={`View your up to date warzone stats below`}
-      />
-      {userData && <StatsGrid profile={userData.profile} />}
-      <Heading
-        headingText={`${username} Recent Matches`}
-        subText={`View your recent warzone matches below`}
-      />
-    </React.Fragment>
-  );
+  if (isLoading) {
+    return (
+      <React.Fragment>
+        <Heading headingText="Loading data..." />
+        <Spinner colorClass="text-green-500" />
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Heading
+          headingText={`${username} Warzone Player Card`}
+          subText={`View your up to date warzone stats below`}
+        />
+        {userData && <StatsGrid profile={userData.profile} />}
+        <Heading
+          headingText={`${username} Recent Matches`}
+          subText={`View your recent warzone matches below`}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default ProfileBox;
