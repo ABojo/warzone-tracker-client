6c964958a59d75a7c4194112946bbf08a428b093
ABojo <alex.bojorquez2598@gmail.com>
2021-06-13 22:10:21 -0400

    Turned the header item into a link component that will redirect the user to the home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <div className="text-white bg-true-gray-800 p-5 flex justify-center items-baseline mb-6">
-      <h1 className="text-2xl inline- font-bold">WZTracker</h1>
+      <Link
+        to="/"
+        className="text-2xl font-bold hover:text-true-gray-300 transition duration-200"
+      >
+        WZTracker
+      </Link>
     </div>
   );
 }
