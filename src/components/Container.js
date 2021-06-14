9859446dfb49da61c253a552ca5db094a03bd5d3
ABojo <alex.bojorquez2598@gmail.com>
2021-06-14 17:16:33 -0400

    removed animation from container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,7 @@
 function Container(props) {
   const { children } = props;
   return (
-    <div className="p-5 w-11/12 max-w-xl mx-auto bg-true-gray-100 rounded shadow fadeInLeft">
+    <div className="p-5 w-11/12 max-w-xl mx-auto bg-true-gray-100 rounded shadow">
       {children}
     </div>
   );
